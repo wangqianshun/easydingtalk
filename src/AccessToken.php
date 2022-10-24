31d6663c68c7ed942f882f4214134ff178f68873
初心 <420029810@qq.com>
2022-10-24 15:21:48 +0800

    first commit

---
+++ src/AccessToken.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace EasyDingTalk;
+
+class AccessToken
+{
+    public static function getToken()
+    {
+
+    }
+}
---
+++ src/config/easydingtalk.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | 钉钉应用设置
+// +----------------------------------------------------------------------
+
+return [
+    /**
+     * TODO 应用基础信息
+     */
+    // 当前应用的id
+    'agent_id' => '1483086443',
+    // 当前应用的app_key
+    'app_key' => 'dingmezsm0prf0ji1wcm',
+    // 当前应用的app_secret
+    'app_secret' => 'Xcnc-dk6ToLM2Wdu5wuRr4ghjYUu1EPUPFE3CAm5xL9e-A8uPRmWHeb0CvIX4gb_',
+
+    /**
+     * TODO 当前应用的access_token信息
+     */
+    // access_token的提前超时重新获取时间，单位秒
+    'access_token_expires' => 180,
+    // access_token的文件路径，建议填写绝对路径
+//    'access_token_file_path' => public_path().'/access_token.json'
+];

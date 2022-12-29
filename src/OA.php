7acea79045e59bcd706fd09fd957686ab4ee519a
初心 <420029810@qq.com>
2022-12-29 17:21:37 +0800

    'oa审批代码继续增加'

--- src/OA.php
+++ src/OA.php
@@ -14,6 +14,7 @@ class OA
     /**
      * 获取表单 schema
      *
+     * @author 初心
      * @param string $processCode
      * @return mixed
      */
@@ -37,6 +38,7 @@ class OA
     /**
      * 创建或修改审批表单模板
      *
+     * @author 初心
      * @param array $body
      * @return mixed
      */
@@ -52,6 +54,7 @@ class OA
     /**
      * 获取审批单流程中的节点信息
      *
+     * @author 初心
      * @param array $body
      * @return mixed
      */
@@ -66,6 +69,7 @@ class OA
     /**
      * 发起审批实例
      *
+     * @author 初心
      * @param array $body
      * @return mixed
      */
@@ -76,6 +80,176 @@ class OA
         // 应用标识AgentId
         $body['microappAgentId'] = Config::getApp()['agent_id'];
 
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 获取单个审批实例详情
+     *
+     * @author 初心
+     * @param string $processInstanceId
+     * @return mixed
+     */
+    public static function getInstance(string $processInstanceId)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances'];
+
+        // 参数
+        $params = [
+            'processInstanceId' => $processInstanceId
+        ];
+
+        // 数据拼接
+        $uri = ApiRequest::joinParams($uri, $params);
+
+        // 发起请求
+        return ApiRequest::get_v1($uri);
+    }
+
+    /**
+     * 转交OA审批任务
+     *
+     * @author 初心
+     * @param array $body
+     * @return mixed
+     */
+    public static function tasksRedirect(array $body)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['tasks_redirect'];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 获取审批钉盘空间信息
+     *
+     * @author 初心
+     * @param string $userId
+     * @return mixed
+     */
+    public static function spaceInfosQuery(string $userId)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['spaces_infos_query'];
+
+        // 参数
+        $body = [
+            // 用户的userId
+            'userId' => $userId,
+            // 应用标识AgentId
+            'agentId' => Config::getApp()['agent_id']
+        ];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 添加审批评论
+     *
+     * @author 初心
+     * @param array $body
+     * @return mixed
+     */
+    public static function comments(array $body)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances_comments'];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 同意或拒绝审批任务
+     *
+     * @author 初心
+     * @param array $body
+     * @return mixed
+     */
+    public static function execute(array $body)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances_execute'];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 撤销审批实例
+     *
+     * @author 初心
+     * @param array $body
+     * @return mixed
+     */
+    public static function terminate(array $body)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances_terminate'];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 授权预览审批附件
+     *
+     * @author 初心
+     * @param array $body
+     * @return mixed
+     */
+    public static function spaceAuthPreview(array $body)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances_spaces_auth_preview'];
+
+        // 应用标识AgentId
+        $body['agentId'] = Config::getApp()['agent_id'];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 授权下载审批钉盘文件
+     *
+     * @author 初心
+     * @param array $body
+     * @return mixed
+     */
+    public static function fileAuthPreview(array $body)
+    {
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances_spaces_files_auth_download'];
+
+        // 发起请求
+        return ApiRequest::post_v1($uri, $body);
+    }
+
+    /**
+     * 下载审批附件
+     *
+     * @author 初心
+     * @param string $processInstanceId
+     * @param string $fileId
+     * @return mixed
+     */
+    public static function fileDownload(string $processInstanceId, string $fileId)
+    {
+
+        // 请求地址
+        $uri = Config::getApi()['oa']['process_instances_spaces_files_urls_download'];
+
+        // 参数
+        $body = [
+            'processInstanceId' => $processInstanceId,
+            'fileId' => $fileId
+        ];
         // 发起请求
         return ApiRequest::post_v1($uri, $body);
     }
